ab530f8fd434bad95eaf037659fc9ae15689acfd
Harald Schult Ulriksen <harald@ulriksen.net>
2020-04-27 09:28:06 +0200

    initial commit

---
+++ SlowSite/Controllers/SlowController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SlowSite.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class SlowController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult FiveSeconds()
+        {
+            Console.WriteLine("start");
+            System.Threading.Thread.Sleep(TimeSpan.FromSeconds(5));
+            Console.WriteLine("got result");
+            return Ok(5);
+        }
+    }
+}
---
+++ ThreadExample/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ThreadExample
+{
+    class Program
+    {
+        private static void logit(string logmesage)
+        {
+            var now = DateTime.Now.ToString();
+            Console.WriteLine($"{now} - {logmesage}");
+        }
+        private static void logline()
+        {
+            Console.WriteLine("----------------------------------------------------------");
+        }
+        static string url = "https://localhost:44392/slow";
+        
+        static async Task Main(string[] args)
+        {
+            logit("warmup");
+            var request = WebRequest.CreateHttp(url);
+            request.Method = "GET";
+            var task = Task.FromResult(request.GetResponse());
+            logit("warmup done");
+            logline();
+            await Plain();
+            logline();
+            await FromResult();
+            logline();
+            await TaskRun();
+            logline();
+            await AwaitTask();
+            logline();
+        }
+
+        static async Task FromResult()
+        {
+            logit("fromresult started");
+            var request = WebRequest.CreateHttp(url);
+            request.Method = "GET";
+            var task = Task.FromResult(request.GetResponse());
+            logit("fromresult task started");
+            var result = await task;
+            logit("fromresult done");
+        }
+
+        static async Task TaskRun()
+        {
+            logit("taskrun started");
+            var request = WebRequest.CreateHttp(url);
+            request.Method = "GET";
+            var task = Task.Run(() => request.GetResponse());
+            logit("taskrun task started");
+            var result = await task;
+            logit("taskrun done");
+        }
+
+        static async Task Plain()
+        {
+            logit("plain started");
+            var request = WebRequest.CreateHttp(url);
+            request.Method = "GET";
+            var result = request.GetResponse();
+            logit("plain done");
+        }
+
+        static async Task AwaitTask()
+        {
+
+            logit("awaittask started");
+            var request = WebRequest.CreateHttp(url);
+            request.Method = "GET";
+            var task = request.GetResponseAsync();
+            logit("awaittask task started");
+            var result = await task;
+            logit("awaittask done");
+
+        }
+    }
+}
